63809c285f0d24bd6f46325ea17ebdc79c771021
BenJensen2 <btjensen@mtu.edu>
2020-06-01 11:01:20 -0700

    Multiplaction Tables

---
+++ Multiplication_Table/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array2D = new int[10,10];
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
