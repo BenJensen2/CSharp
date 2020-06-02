0fdf771c58c3a123db4f48398cb9361b63cf64ea
BenJensen2 <btjensen@mtu.edu>
2020-06-01 20:01:40 -0700

    Puzzles

--- Puzzles/Program.cs
+++ Puzzles/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Puzzles
 {
@@ -25,9 +26,57 @@ namespace Puzzles
             }
             Console.WriteLine("Max: " + max + " Min " + min + " Sum: " + sum);
         }
+
+        public static int TossCoin(){
+            // Console.WriteLine("Tossing a Coin!");
+            Random num = new Random();
+            int result = num.Next(1,3);
+            // string resultStr = "Neither";
+            if (result==1){
+                // resultStr = "Heads";
+                // Console.WriteLine(resultStr);
+            }
+            else{
+                // resultStr = "Tails";
+                // Console.WriteLine(resultStr);
+            }
+            // Console.WriteLine(result);
+            return result;
+        }
+
+        public static double TossMultipleCoins(int num){
+            int headCount = 0;
+            int tailCount = 0;
+            for (int val=0; val<num; val++){
+                int result = TossCoin();
+                if (result==1){
+                    headCount ++;
+                }
+                if (result==2){
+                    tailCount ++;
+                }      
+                Console.WriteLine(headCount + " : " + tailCount + " num: " + num);      
+            }
+            Console.WriteLine(headCount);
+            double ratio = 100*headCount/num;
+            Console.WriteLine(ratio + "%");
+            return ratio;
+        }
+
+        public static string Names()
+        {
+            List <string> names = new List<string>();
+            names.Add("Todd");
+            names.Add("Tiffany");
+            names.Add("Charlie");
+            names.Add("Geneva");
+            names.Add("Sydney");
+        }
         static void Main(string[] args){
             Console.WriteLine("Hello World!");
             RandomArray();
+            TossCoin();
+            TossMultipleCoins(22);
         }
     }
 }
