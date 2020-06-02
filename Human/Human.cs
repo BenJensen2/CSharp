706e356858d25d617799d1b7f8bce25b9103fee9
BenJensen2 <btjensen@mtu.edu>
2020-06-02 14:19:44 -0700

    Human

---
+++ Human/Human.cs
@@ -0,0 +1,35 @@
+namespace Humans
+{
+    public class Human
+    {
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health;
+        
+        public int Health {get;}
+
+        public Human(string name){
+            Name = name;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            health = 100;
+        }
+
+        public Human(string name, int strength, int intelligence, int dexterity, int health){
+            Name = name;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            health = 100;
+        }
+
+        public int Love(Human target){
+            target.health += 5*target.Strength;
+            return health;
+        }
+
+    }
+}
---
+++ Human/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Humans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Human Dan = new Human("Dan");
+            Console.WriteLine(Dan.Love(Dan));
+        }
+    }
+}
