015a00698e914c4138bd3c806def37148007a17f
BenJensen2 <btjensen@mtu.edu>
2020-06-06 19:00:50 -0700

    Dojo Survey

---
+++ Dojo_Survey/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dojo_Survey.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public ViewResult Survey()
+        {
+            ViewBag.trello = "Sadie";
+            return View();
+        }
+
+        [HttpGet("/result")]
+        public ViewResult Result()
+        {
+            return View();
+        }
+
+        // [HttpGet("/travel/{destination}")]
+        // public RedirectToActionResult NotSure(string destination)
+        // {
+        //     Console.WriteLine($"\n mike: {destination} \n");
+
+        //     return RedirectToAction("Result");
+        // }
+
+        // [HttpGet("/travel2/{destination}")]
+        // public RedirectToActionResult NotSure(string destination)
+        // {
+        //     Console.WriteLine($"\n mike Hello: {destination} \n");
+
+        //     return RedirectToAction("Result");
+        // }
+
+        [HttpGet("/redirect")]
+        public RedirectToActionResult Thello()
+        {
+            return RedirectToAction("Result");
+        }
+
+
+
+    }
+}
---
+++ Dojo_Survey/Models/Ninja.cs
@@ -0,0 +1,9 @@
+namespace Dojo_Survey.Models
+{
+    public class Ninja
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Language { get; set; }
+    }
+}
